cb53d7ccb73b6eddb9f8055d6f9a5555b35abcf3
Jan Suhr <jan@nitrokey.com>
2026-04-09 18:25:38 +0200

    [FIX] project_timesheet_time_control: remove property view_type

--- project_timesheet_time_control/models/account_analytic_line.py
+++ project_timesheet_time_control/models/account_analytic_line.py
@@ -107,7 +107,6 @@ class AccountAnalyticLine(models.Model):
             "target": "new",
             "type": "ir.actions.act_window",
             "view_mode": "form",
-            "view_type": "form",
         }
 
     def button_end_work(self):
